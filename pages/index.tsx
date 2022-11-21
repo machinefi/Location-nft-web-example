83534d60a5e8ce1eb6655678e01272b9e0cf1dc2
jane <yinyimingdodo@163.com>
2022-11-21 20:15:27 +0800

    update timestamp

--- pages/index.tsx
+++ pages/index.tsx
@@ -79,8 +79,8 @@ export default function Home() {
         signature,
         message, 
         owner: address,
-        from: moment().startOf('day').valueOf(),
-        to: moment().endOf('day').valueOf()
+        from: `${moment().startOf('day').valueOf()}`,
+        to: `${moment().endOf('day').valueOf()}`
       })    
       console.log('response', response)
       const result = response.data.result
