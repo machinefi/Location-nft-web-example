0e0608f82fac8e4d44daace52737040a56badf84
DLHTX <949729789@qq.com>
2023-02-23 16:39:47 +0800

    downgrade puppeteer

--- src/pages/api/image/[id].ts
+++ src/pages/api/image/[id].ts
@@ -3,7 +3,7 @@ import path from "path";
 
 export default function handler(req, res) {
   const { id } = req.query;
-  const filePath = path.join(process.cwd(), "public", "images", `${id}.png`);
+  const filePath = path.join(process.cwd(), "var", "image", `${id}.png`);
   const file = fs.readFileSync(filePath);
   res.setHeader("Content-Type", "image/jpeg");
   res.send(file);
